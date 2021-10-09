030365819752d0d8a3c1d475fc35030351f6755f
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-10-08 22:18:27 -0300

    Update Teclado.java

--- br.com.raldineyr.calculadora.visao/Teclado.java
+++ br.com.raldineyr.calculadora.visao/Teclado.java
@@ -1,14 +1,17 @@
 package br.com.raldineyr.calculadora.visao;
 
-
 import java.awt.Color;
+
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class Teclado extends JPanel {
+public class Teclado extends JPanel implements ActionListener {
 	
 	private final Color COR_CINZA_ESCURO = new Color(68, 68, 68);
 	private final Color COR_CINZA_CLARO = new Color(99, 99, 99);
@@ -25,23 +28,24 @@ public class Teclado extends JPanel {
 		c.weighty = 1;
 		c.fill = GridBagConstraints.BOTH;
 		
-		// LINHA 1
-		c.gridwidth = 3;
+		// LINHA 1 
+		c.gridwidth = 2;
 		adicionarBotao("AC",COR_CINZA_ESCURO, c, 0,0);
 		c.gridwidth = 1;
+		adicionarBotao("±",COR_CINZA_ESCURO, c, 2,0);
 		adicionarBotao("/",COR_LARANJA,c, 3, 0);
 		
 		// LINHA 2
 		adicionarBotao("7",COR_CINZA_ESCURO, c, 0,1);
 		adicionarBotao("8",COR_CINZA_ESCURO, c, 1,1);
 		adicionarBotao("9",COR_CINZA_ESCURO, c, 2,1);
-		adicionarBotao("X",COR_LARANJA,c, 3, 1);
+		adicionarBotao("*",COR_LARANJA,c, 3, 1);
 		
 		// LINHA 3
 		adicionarBotao("4",COR_CINZA_ESCURO, c, 0,2);
 		adicionarBotao("5",COR_CINZA_ESCURO, c, 1,2);
 		adicionarBotao("6",COR_CINZA_ESCURO, c, 2,2);
-		adicionarBotao("–",COR_LARANJA,c, 3, 2);
+		adicionarBotao("-",COR_LARANJA,c, 3, 2);
 		
 		// LINHA 4
 		adicionarBotao("1",COR_CINZA_ESCURO, c, 0,3);
@@ -65,7 +69,18 @@ public class Teclado extends JPanel {
 		c.gridy = y;
 		
 		Botao botao = new Botao(texto, cor);
+		botao.addActionListener(this);
 		add(botao, c);
 	}
-	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if (e.getSource() instanceof JButton) {
+		JButton botao = (JButton) e.getSource();
+		//System.out.println(botao.getText());
+		Memoria.getInstancia().processarComando(botao.getText());
+		
+		}
+	}	
 }
