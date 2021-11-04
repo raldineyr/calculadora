e291e06e8212f8cc0005a8ef095ccb3bc3517a64
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-11-04 09:11:59 -0300

    Update TipoComando.java

--- br.com.raldineyr.calculadora.visao/TipoComando.java
+++ br.com.raldineyr.calculadora.visao/TipoComando.java
@@ -2,5 +2,5 @@ package br.com.raldineyr.calculadora.visao;
 
 public enum TipoComando {
 		
-	ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA, INVERTER;
+	ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA, INVERTER, PERCENT;
 }
